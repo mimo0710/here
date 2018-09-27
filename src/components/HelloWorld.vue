38d3d42593daadba9ab9401b49960f861883a87e
mimo <931750817@qq.com>
2018-09-27 15:47:57 +0800

    help me

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <el-tabs v-model="activeName">
+    <el-tab-pane label="快讯生成" name="first">
+      <el-row>
+
+        <ul>
+          <li v-for="item in number" :key="item">
+            <el-checkbox :label="item">
+              <div class="title"></div>
+              <el-input
+              :rows="12"
+              :placeholder="hello"
+              clearable>
+              </el-input>
+            </el-checkbox>
+          </li>
+        </ul>
+
+      </el-row>
+      <el-button-group>
+        <el-button type="primary" icon="el-icon-refresh" size="medium"></el-button>
+        <el-button type="primary" icon="el-icon-download" size="medium"></el-button>
+      </el-button-group>
+    </el-tab-pane>
+    <el-tab-pane label="参数配置" name="second">参数配置</el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return{
+      activeName: 'first',
+      number:[1,2,3,4,5]
+    }
+  },
+  methods: {
+   axios.post('/user?ID=12345')
+   .then(function (response) {
+      console.log(response)
+   })
+  
+  }
+
+}
+</script>
+
+<style scoped>
+ul,li{
+  list-style:none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-bottom: 30px;
+}
+
+</style>
